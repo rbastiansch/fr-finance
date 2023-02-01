618bdc18893e66201588ceec14d54f668fea4067
rbastiansch <rafaelbastiansch@gmail.com>
2023-02-01 02:05:54 -0300

    feat: make input full width

--- front/components/Transactions/TransactionsFilter.vue
+++ front/components/Transactions/TransactionsFilter.vue
@@ -3,7 +3,7 @@
     <div class="text-slate-500">Search</div>
     <input
       type="text"
-      class="placeholder-shown:border-gray-500 border py-1 px-2"
+      class="w-full placeholder-shown:border-gray-500 border py-1 px-2"
       placeholder="Search by bank, account, reference, category, date, amount, currency"
       :value="value"
       @input="input"
